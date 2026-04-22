182bfe9cbedd04e3e9985a3e1ed3cf8f9f191ca5
Revanth Kanagala <revanthchowdary2002@gmail.com>
2026-04-22 13:55:04 -0400

    Added deleteTask method - closes #1 - Madan Datta

--- TaskManager.py
+++ TaskManager.py
@@ -6,3 +6,11 @@ class TaskManager:
 
     def addTask(self, title, priority):
         self.tasks.append({"title": title, "priority": priority})
+
+
+def deleteTask(self, task_id):
+    """
+    Deletes a task based on its ID
+    """
+    if 0 <= task_id < len(self.tasks):
+        self.tasks.pop(task_id)
